2dbc62598e37e3cf5b90038ad02a5da3b03ee3bf
Prachi Kumari <Prachi.Kumari@hyland.com>
2025-10-31 20:21:51 +0530

    Populated entities with field

--- EmployeeManagementSystem/EMS.Domain/Entities/Employees.cs
+++ EmployeeManagementSystem/EMS.Domain/Entities/Employees.cs
@@ -1,12 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace EMS.Domain.Entities
 {
-    public class Employees
-    {
-    }
+        public class Employee : BaseEntity
+        {
+            public int EmployeeID { get; set; }
+
+            public string FirstName { get; set; } = string.Empty;
+
+            public string LastName { get; set; } = string.Empty;
+
+            public string ContactNo { get; set; } = string.Empty;
+
+            public DateTime DateOfBirth { get; set; }
+
+            public int DepartmentID { get; set; }
+
+            public int RoleID { get; set; }
+
+            public DateTime HireDate { get; set; }
+
+            public decimal Salary { get; set; }
+
+            public int? ManagerID { get; set; }
+
+            public bool IsActive { get; set; }
+
+            public int UserID { get; set; }
+
+            public Departments? Department { get; set; }   
+            public Roles? Role { get; set; }               
+            
+        }
+
+
 }
--- EmployeeManagementSystem/EMS.Domain/Entities/UserRole.cs
+++ EmployeeManagementSystem/EMS.Domain/Entities/UserRole.cs
@@ -1,12 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace EMS.Domain.Entities
 {
-    public class UserRole
-    {
-    }
+       
+       public class UserRole : BaseEntity
+        {
+            public int UserRoleID { get; set; }
+
+            public int UserID { get; set; }
+
+            public int RoleID { get; set; }
+
+            public bool IsActive { get; set; }
+
+          
+            public Users? User { get; set; }   
+            public Roles? Role { get; set; }   
+        }
+    
+
+
 }
