8c9a0fc749f3a24d79bd1e54caea3d60bc047eb7
Prachi Kumari <Prachi.Kumari@hyland.com>
2025-11-03 18:20:04 +0530

    Adding DTOs for UserController

---
+++ EmployeeManagementSystem/EMS.Domain/DTO/AddUserRequestDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EMS.Domain.DTO
+{
+    public class AddUserRequestDTO
+    {
+        public int UserID { get; set; }
+
+        public string UserName { get; set; } = string.Empty;
+
+        public string EmailID { get; set; } = string.Empty;
+    }
+}
